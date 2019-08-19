09f4026760aa522cb30dc06fcf0862c79b708617
Benoit Parmentier <benoit.parmentier@gmail.com>
2019-08-19 14:30:00 -0400

    setting up workflow with data base query

--- postgis_plasticemission_data_manipulation.R
+++ postgis_plasticemission_data_manipulation.R
@@ -2,7 +2,7 @@
 ## This script sets up the modeling pipeline for the plasticemission database
 ## 
 ## DATE CREATED: 08/16/2019
-## DATE MODIFIED: 08/19/2019
+## DATE MODIFIED: 08/20/2019
 ## AUTHORS: Benoit Parmentier, Stephanie Borelle
 ## PROJECT: plasticemission working group
 ## ISSUE: accessing database and data imputation set up 
@@ -23,19 +23,12 @@
 library(gtools)                              # loading some useful tools 
 library(sp)                                  # Spatial pacakge with class definition by Bivand et al.
 library(sf)                                  # Spatial package with OGC standard definition, spatial object as df
-library(spdep)                               # Spatial pacakge with methods and spatial stat. by Bivand et al.
 library(rgdal)                               # GDAL wrapper for R, spatial utilities
 library(gdata)                               # various tools with xls reading, cbindX
-library(raster)                              # Raster and image processing in R
-library(rasterVis)                           # Raster plotting functions
 library(parallel)                            # Parallelization of processes with multiple cores
 library(maptools)                            # Tools and functions for sp and other spatial objects e.g. spCbind
 library(maps)                                # Tools and data for spatial/geographic objects
 library(plyr)                                # Various tools including rbind.fill
-library(spgwr)                               # GWR method
-library(rgeos)                               # Geometric, topologic library of functions
-library(gridExtra)                           # Combining lattice plots
-library(colorRamps)                          # Palette/color ramps for symbology
 library(ggplot2)
 library(lubridate)
 library(dplyr)
@@ -70,11 +63,9 @@ load_obj <- function(f){
 
 ### Other functions for later ####
 
+###This is where you can put the function 
 #function_processing <- ".R" #PARAM 1
-
-#script_path <- "/nfs/bparmentier-data/Data/projects/supplychaincommitments/scripts"
-#script_path <- "/nfs/supplychaincommitments-data/scripts"
-
+#script_path <- "/nfs/PlasticEmission-data/scripts"
 #source(file.path(script_path,function_processing)) #source all functions used in this script 1.
 
 ############################################################################
@@ -86,7 +77,7 @@ out_dir <- "/nfs/PlasticEmission-data/tmp"
 num_cores <- 2 #param 8 #normally I use only 1 core for this dataset but since I want to use the mclappy function the number of cores is changed to 2. If it was 1 then mclappy will be reverted back to the lapply function
 create_out_dir_param=TRUE # param 9
 
-out_suffix <-"postgis_data_08162019" #output suffix for the files and ouptut folder #param 12
+out_suffix <-"postgis_data_08202019" #output suffix for the files and ouptut folder #param 12
 
 ### db related:
 #db_ini_filename <- "/nfs/bparmentier-data/.pg_service.conf" #password info
@@ -124,20 +115,10 @@ options(scipen=999)  #remove scientific writing
 ## 2) connect to database: bparmentier@sshgw02:~$ psql -h postgis02.research.sesync.org -d plasticemission -U plasticemission
 
 ## 4) FROM R:
-
-#Load data into PostgreSQL from ESRI shape file 
-##Read in password: 
-db_password <- scan(db_ini_filename, character(), n = 1)
-
-
 ## Use /.pg_service.conf for better practice
-#conn <- dbConnect(Postgres(), 
-#                  service = 'plasticemission')
- 
-#Load data into PostgreSQL from ESRI shape file 
+
 ##Read in password: 
 db_password <- scan(db_ini_filename, character(), n = 1)
-
 conn <- dbConnect(PostgreSQL(), 
                  dbname = db_name,
                  user = db_user,
@@ -161,16 +142,15 @@ region_subset <- dbGetQuery(conn, sql_command) #this is a data.frame
 region_subset
 
 ## Select the subset of data your interested in by location
-
 sql_command <- "SELECT * FROM location"
 location <- dbGetQuery(conn, sql_command)
-location
+class(location) # This is a data.frame
 
 sql_command <- "SELECT COUNT(*) FROM value"
 no_row <- dbGetQuery(conn, sql_command) #this is a data.frame
 sql_command <- "SELECT COUNT(DISTINCT(location_id)) FROM value"
 no_location <- dbGetQuery(conn, sql_command)
-no_location
+no_location #number of locations
 
 sql_command <- "SELECT * FROM value"
 value <- dbGetQuery(conn, sql_command) #this is a data.frame
@@ -182,44 +162,42 @@ value_join <- dbGetQuery(conn, sql_command) #this is a data.frame
 value_join
 dim(value_join)
 
+### Writing out table in output directory defined earlier
 write.table(value_join,file.path(out_dir,"value_join.txt"),sep=",")
 
-sql_command <- "COPY temperature TO '/nfs/PlasticEmission-data/tmp/value_join2.csv' DELIMITER ',' CSV HEADER;"
-test <- dbGetQuery(conn, sql_command) #this is a data.frame
+### Now this how to extract data by country and variable:
+sql_command <- "SELECT * FROM value JOIN location ON location_id = location.id"
+value_df <- dbGetQuery(conn, sql_command) #this is a data.frame
+dim(value_df)
+list_countries <- unique(value_df$name)
+length(list_countries)
+dim(value_df)
+#View(value_df)
 
-#> test <- dbGetQuery(conn, sql_command) #this is a data.frame
-#Error in postgresqlExecStatement(conn, statement, ...) : 
-#  RS-DBI driver: (could not Retrieve the result : ERROR:  must be superuser to COPY to or from a file
-#                  HINT:  Anyone can COPY to stdout or from stdin. psql's \copy command also works for anyone.
-#)
-#Warning message:
-#In postgresqlQuickSQL(conn, statement, ...) :
-#  Could not create execute: COPY temperature TO '//nfs/PlasticEmission-data/tmp/value_join2.csv' DELIMITER ',' CSV HEADER;
+##### Example for variable 
+i <- 1
+country_name <- list_countries[i]
+varipasable_id <- 25
 
-#COPY temperature TO '/home/bparmentier/Data/Benoit/Databases/postgres/outputs/temperature.csv' DELIMITER ',' CSV HEADER;
+sql_command <- paste("SELECT * FROM value JOIN location ON location_id=location.id ", 
+                     "WHERE name=",shQuote(country_name)," ",
+                     "AND variable_id = ",variable_id, ";",sep="")
 
-sql_command <- "SELECT * FROM value JOIN location ON location_id = location.id WHERE name = 'Estonia' and variable_id = 25"
-value_estonia <- dbGetQuery(conn, sql_command) #this is a data.frame
-class(value_estonia)
-
-## imputation
-
-### modeling 
-
+data_df <- dbGetQuery(conn, sql_command)  #Selecting station using a SQL query
 ## output dir
-write.table(value_estonia,file.path(out_dir,"value_estonia.txt"),sep=",")
 
-#SELECT name FROM (SELECT name FROM agentinformation) as a  
+out_filename <- paste0("data_extracted_",country_name,".csv")
+write.table(data_df,file.path(out_dir,out_filename),sep=",")
 
-sql_command <- "SELECT id, year,value FROM (SELECT * FROM value JOIN location ON location_id = location.id WHERE name = 'Estonia' and variable_id = 25) AS estonia"
-sql_command <- "SELECT id, year,value FROM (SELECT * FROM value JOIN location ON location_id = location.id WHERE name = 'Estonia' and variable_id = 25) AS estonia"
-
-value_estonia2 <- dbGetQuery(conn, sql_command) #this is a data.frame
-class(value_estonia2)
-
-write.table(value_estonia,file.path(out_dir,"value_estonia.txt"),sep=",")
 
 dbDisconnect(conn)
+
+### Stage 2: imputation
+
+### Stage 3: modeling 
+
+### Stage 4: evaluation
+
 
 #################### End of script ##################################
 
